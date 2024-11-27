97ca6ff4d2d288bf44376dd6e472efb737af5703
wangyiyang7 <wang.yiyang7@northeastern.edu>
2024-11-26 19:39:11 -0800

    Update index.ts

--- server/api/index.ts
+++ server/api/index.ts
@@ -2,9 +2,7 @@
 
 const express = require('express');
 const app = express();
-onst jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const cors = require("cors");
+
 
 app.get("/", async (req, res) => {
   try {
